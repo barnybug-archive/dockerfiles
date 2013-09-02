86a53e2827cf7ec8962b4a7ff7ed5775d49f1859
Barnaby Gray <barnaby.gray@artirix.com>
2013-09-02 13:59:48 +0100

    Cleaner start/stop of mysqld

--- mysql-server/mysql-setup.sh
+++ mysql-server/mysql-setup.sh
@@ -3,5 +3,12 @@
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 
 /usr/sbin/mysqld &
-sleep 5
+
+echo 'Waiting for mysqld to startup...'
+while ! mysqladmin ping 2>/dev/null; do sleep 1; done
+
 echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
+echo 'Shutting down mysqld...'
+mysqladmin shutdown
+while pidof mysqld; do sleep 1; done
