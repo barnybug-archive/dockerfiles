7ff2008e5b6d5543ce62df949956d503ced80f85
Barnaby Gray <barnaby.gray@artirix.com>
2014-02-10 22:31:52 +0000

    Lighter weight mysqld install - avoid perl!

--- mysql-server/mysql-setup.sh
+++ mysql-server/mysql-setup.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/bash -ex
 
-sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+useradd mysql
+chmod 700 /etc/mysql/my.cnf
+mkdir -p /etc/mysql/conf.d /var/run/mysqld
+
+/usr/bin/mysql_install_db --force
+chown mysql:mysql /var/lib/mysql /var/run/mysqld
 
 /usr/sbin/mysqld &
 
 echo 'Waiting for mysqld to startup...'
-while ! mysqladmin ping 2>/dev/null; do sleep 1; done
+while ! mysql -e '' 2>/dev/null; do sleep 1; done
 
 echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
 
 echo 'Shutting down mysqld...'
-mysqladmin shutdown
+kill `pidof mysqld`
 while pidof mysqld; do sleep 1; done
